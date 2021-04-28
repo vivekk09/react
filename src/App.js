ada08528240e56c39f59a832816267816cd74e69
Vivek Kumar Shrivastava <shrivastava.96vivek@gmail.com>
2021-04-28 11:13:15 +0530

    update : emoji app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { useState } from "react";
 import "./styles.css";
 
-export default function App() {
-  const [likeCount, setlikeCount] = useState(0);
+const emojiDictionary = {
+  "😊": "Smiling"
+};
 
-  function likehandler() {
-    var newlikeCount = likeCount + 1;
-    setlikeCount(newlikeCount);
+export default function App() {
+  const [emoji, setEmoji] = useState("");
+
+  function emojiHandler(event) {
+    var userEmoji = event.target.value;
+    var result = emojiDictionary[userEmoji];
+
+    if (result === undefined) {
+      result = "We don't have this emoji in our database!";
+    }
+    setEmoji(result);
   }
 
   return (
     <div className="App">
       <h1>Interpret</h1>
-      <button onClick={likehandler}>Like Me!</button> {likeCount}
+      <input onChange={emojiHandler}></input>
+      <div> Interpretation: {emoji} </div>
     </div>
   );
 }
