b8d2635494c1a2d48ee5b17d49bed443d419e151
Vivek Kumar Shrivastava <shrivastava.96vivek@gmail.com>
2021-04-29 02:00:02 +0530

    completed: react app

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React, { useState } from "react";
 import "./styles.css";
 
 const emojiDictionary = {
-  "😊": "Smiling"
+  "😊": "Smiling",
+  "😜": "Wink"
 };
 
 export default function App() {
   const [emoji, setEmoji] = useState("");
+
+  var ouremojis = Object.keys(emojiDictionary);
+
+  function emojiClickHandler(emoji) {
+    var result = emojiDictionary[emoji];
+    setEmoji(result);
+  }
 
   function emojiHandler(event) {
     var userEmoji = event.target.value;
@@ -23,6 +31,17 @@ export default function App() {
       <h1>Interpret</h1>
       <input onChange={emojiHandler}></input>
       <div> Interpretation: {emoji} </div>
+
+      <h3>Emojis we have:</h3>
+      {ouremojis.map((emoji) => (
+        <span
+          onClick={() => emojiClickHandler(emoji)}
+          style={{ cursor: "pointer", fontSize: "2rem" }}
+          key={emoji}
+        >
+          {emoji}{" "}
+        </span>
+      ))}
     </div>
   );
 }
